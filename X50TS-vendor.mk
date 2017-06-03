92aaff3c354b5fc5e0c20931fb210ddc03828d94
garikdan <garikdan@mail.ru>
2017-06-03 17:07:15 +0300

    proprietary HaseeX50_TS

---
+++ X50TS-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/HASEE/X50TS/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/HASEE/X50TS/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/HASEE/X50TS/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/HASEE/X50TS/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/HASEE/X50TS/proprietary/vendor,system/vendor)
